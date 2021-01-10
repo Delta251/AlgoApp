9d9fa1ce2d520ce3fddfca5d4640f3450177d463
maxi <maxi_S15@yahoo.de>
2021-01-10 21:00:02 +0100

    Base Xamarin App, learning from here

---
+++ AlgoApp/AlgoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AlgoApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AlgoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
